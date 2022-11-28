a7e0550635191d04c881ea0e1af8c2023b1a2bad
taasha10 <sharmapratyasha1@gmail.com>
2022-11-28 03:40:01 -0500

    styled image and changed to card

--- src/movie/movie-list-item.js
+++ src/movie/movie-list-item.js
@@ -13,26 +13,30 @@ const MovieListItem = (
 ) => {
 
     return(
-            <div className="row border-dark mt-2">
-                <div className="col-3">
-                    <img width={200} className="float-end rounded-3" src={movie.image} alt={`movie thumbnail`}/>
-                </div>
-                <div className="col-9 p-2 mt-2 bg-light">
-                    <div className={'h4'}>{movie.title}</div>
-                    <div className={`text-muted`}>by {movie.director}</div>
-                    <div className={'pt-3'}>
-                        <span className={"text-warning"}>
-                        {[...Array(movie.ratings)].map(i => <i className="fa-solid fa-star pe-1"/>
-                        ) }
-                        </span>
-                        <span className={"text-warning"}>
-                        {[...Array(5-movie.ratings)].map(i => <i className="fa-regular fa-star pe-1"/>
-                        ) }
-                        </span>
-                        <span className={'text-secondary ps-3'}>{movie.votes} votes</span>
+            <div className="card border-0 mt-2">
+                <div className={'card-body bg-light rounded-2'}>
+                    <div className="card-img">
+                        <img width={"200"} className="float-start wd-thumbnail" src={movie.image} alt={`movie thumbnail`}/>
                     </div>
-                    <div className={'text-success pt-3'}>{movie.description}</div>
+                    <div className="p-2 mb-3 wd-float-right">
+                        <div className={'h4 card-title'}>{movie.title}</div>
+                        <div className={`card-subtitle text-muted`}>by {movie.director}</div>
+                        <div className={'pt-3'}>
+                            <span className={"text-warning"}>
+                            {[...Array(movie.ratings)].map(i => <i className="fa-solid fa-star pe-1"/>
+                            ) }
+                            </span>
+                            <span className={"text-warning"}>
+                            {[...Array(5-movie.ratings)].map(i => <i className="fa-regular fa-star pe-1"/>
+                            ) }
+                            </span>
+                            <span className={'text-secondary ps-3'}>{movie.votes} votes</span>
+                        </div>
+                        <div className={'card-text text-success pt-3'}>{movie.description}</div>
+                    </div>
                 </div>
+
+
             </div>
 
     );
