3108ed654c79facdbccf27eec009b3427948d0b5
taasha10 <sharmapratyasha1@gmail.com>
2022-11-27 23:08:39 -0500

    changed navigation style

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,27 +1,29 @@
 import React from "react";
 import { useLocation, Link } from "react-router-dom";
+import './index.css';
 
 function Navigation() {
   const { pathname } = useLocation();
   const links = [
-    { label: 'Action', icon: 'fa-home', path: '/action' },
-    { label: 'Horror', icon: 'fa-home', path: '/horror' },
-    { label: 'Thriller', icon: 'fa-home', path: '/thriller' },
-    { label: 'Fiction', icon: 'fa-home', path: '/fiction' },
-    { label: 'Sci-Fi', icon: 'fa-home', path: '/sci-fi' },
-    { label: 'Love & Romance', icon: 'fa-home', path: '/romance' },
-    { label: 'Kids & family', icon: 'fa-home', path: '/kids-and-family' },
+    { label: 'Action', icon: 'fa-home', image:'../../images/action.png', path: '/action' },
+    { label: 'Horror', icon: 'fa-home', image:'../../images/horror.png', path: '/horror' },
+    { label: 'Thriller', icon: 'fa-home', image:'../../images/thriller.png', path: '/thriller' },
+    { label: 'Fiction', icon: 'fa-home', image:'../../images/fiction.png', path: '/fiction' },
+    { label: 'Sci-Fi', icon: 'fa-home', image:'../../images/sci-fi.png', path: '/sci-fi' },
+    { label: 'Love & Romance', icon: 'fa-home', image:'../../images/romance.png', path: '/romance' },
+    { label: 'Kids & family', icon: 'fa-home', image:'../../images/family.png', path: '/kids-and-family' },
   ];
   return (
-    <ul className="list-group">
+    <ul className="nav nav-pills flex-column">
       {
         links.map((link, ndx) => {
           return (
-            <li key={ndx} className="list-group-item border-0">
+            <li key={ndx} className="nav-item">
               <Link
                 to={link.path} id={link.label}
-                className={`text-nowrap ${pathname.indexOf(link.path) >= 0 ? 'active' : ''}`}>
-                <i className={`fa ${link.icon} text-center`}></i>
+                className={`text-nowrap nav-link ${pathname.indexOf(link.path) >= 0 ? 'active' : ''}`}>
+                <img src={link.image} className={'rounded-circle'} width={30} height={30}/>&nbsp;&nbsp;
+                {/*<i className={`fa ${link.icon} text-center`}/>*/}
                 <span className="ttr-label">{link.label}</span>
               </Link>
             </li>
