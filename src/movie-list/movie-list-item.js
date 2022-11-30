a58c405a84a0c6ea62c14839f9d083eb075c0f6b
Saket Kumar <kumar.sak@northeastern.edu>
2022-11-30 00:10:06 -0500

    Added hover style css transtions

--- src/movie-list/movie-list-item.js
+++ src/movie-list/movie-list-item.js
@@ -3,7 +3,7 @@ import Rating from "./rating";
 const MovieListItem = ({ movie }) => {
     return (
         <div className="mv-card pt-3">
-            <div className="position-relative bg-white p-15 rounded-3">
+            <div className="position-relative p-15 rounded-3">
                 <div className="d-flex flex-row">
                     <div className="thumbnail-holder position-relative">
                         <div className="card-img position-absolute rounded-3">
@@ -11,7 +11,7 @@ const MovieListItem = ({ movie }) => {
                         </div>
                     </div>
                     <div className="movie-content ms-3 pt-2">
-                        <p className="m-0">{movie.title}</p>
+                        <p className="m-0 text-black">{movie.title}</p>
                         <p className="m-0 text-muted"><small>By: {movie.director}</small></p>
                         <p className="m-0 text-muted"><small><i className="fa-solid text-warning fa-star pe-1" /> {movie.ratings}</small></p>
                         <div className="mt-2">
