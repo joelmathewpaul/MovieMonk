fbb2a7dc812d7b1b6b450a89eebcd4154db5a269
taasha10 <sharmapratyasha1@gmail.com>
2022-12-08 17:44:53 -0500

    update model to match backend

--- src/models/user.js
+++ src/models/user.js
@@ -3,20 +3,24 @@
  * user profile.
  */
 export default class User {
-    constructor(id, name, email, profilePicture, dateOfBirth, dateJoined) {
+    constructor(id, name, email, profilePhoto,headerImage,biography, dob, joined,accountType,followersCount,followingCount) {
         if (this.email === null || this.name === null) {
             throw new Error('Name cannot be empty');
         }
         this.id = id;
         this.name = name;
         this.email = email;
-        this.profilePicture = profilePicture;
-        this.dateOfBirth = dateOfBirth;
-        this.dateJoined = dateJoined;
+        this.profilePhoto = profilePhoto;
+        this.headerImage = headerImage;
+        this.dob = dob;
+        this.joined = joined;
+        this.accountType=accountType;
+        this.followersCount = followersCount;
+        this.followingCount = followingCount;
     }
 
     static getUserDetails(res) {
-        return new User(res._id, res.name, res.email, res.profilePicture, res.dateOfBirth, res.dateJoined);
+        return new User(res._id, res.name, res.email, res.profilePhoto, res.headerImage,res.dob, res.joined, res.accountType,res.followersCount,res.followingCount);
 
     }
 }
