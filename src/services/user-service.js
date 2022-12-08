e12ab7e61841c99e9ed9efb48b4681fc1c04ccca
taasha10 <sharmapratyasha1@gmail.com>
2022-12-08 17:54:27 -0500

    added axios to api

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const USER_API = `${API_BASE}/user`;
-const api = axios;
+const api = axios({
+                      withCredentials : true
+                  }
+);
 
 /**
  * Finds user profile when user clicks on "My Profile".
