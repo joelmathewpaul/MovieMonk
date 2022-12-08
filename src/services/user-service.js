5fdb5b5c84152a4d33bd2e3f0816b3c6a9e984fc
taasha10 <sharmapratyasha1@gmail.com>
2022-12-08 17:23:17 -0500

    removed delete,create user from service

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -2,15 +2,6 @@ import axios from 'axios';
 const API_BASE = process.env.REACT_APP_API_BASE;
 const USER_API = `${API_BASE}/user`;
 
-/**
- * Creates user on sign up. TODO: use this method upon sign up
- * @param user
- * @returns {Promise<any>}
- */
-export const createUser = async (user) => {
-    const response = await axios.post(USER_API, user)
-    return response.data;
-}
 
 /**
  * Finds user profile when user clicks on "My Profile".
@@ -22,16 +13,6 @@ export const findUser = async () => {
     return user;
 }
 
-/**
- * Delete user account using unique id. TODO
- * @param uid
- * @returns {Promise<any>}
- */
-export const deleteUser = async (uid) => {
-    const response = await axios
-        .delete(`${USER_API}/${uid}`)
-    return response.data
-}
 
 /**
  * Update Updates the user info. todo: need to be called when the user clicks on edit-profile.
