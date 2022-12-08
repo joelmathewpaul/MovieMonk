1fd2e3f74a83aa12da8c9256c10a18f8b4a95630
Saket Kumar <kumar.sak@northeastern.edu>
2022-12-08 18:15:35 -0500

    Fixed the biography error

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@
  * user profile.
  */
 export default class User {
-    constructor(id, name, email, profilePhoto,headerImage,biography, dob, joined,accountType,followersCount,followingCount) {
+    constructor(id, name, email, profilePhoto, headerImage, biography, dob, joined, accountType, followersCount, followingCount) {
         if (this.email === null || this.name === null) {
             throw new Error('Name cannot be empty');
         }
@@ -12,15 +12,16 @@ export default class User {
         this.email = email;
         this.profilePhoto = profilePhoto;
         this.headerImage = headerImage;
+        this.biography = biography;
         this.dob = dob;
         this.joined = joined;
-        this.accountType=accountType;
+        this.accountType = accountType;
         this.followersCount = followersCount;
         this.followingCount = followingCount;
     }
 
     static getUserDetails(res) {
-        return new User(res._id, res.name, res.email, res.profilePhoto, res.headerImage,res.dob, res.joined, res.accountType,res.followersCount,res.followingCount);
+        return new User(res._id, res.name, res.email, res.profilePhoto, res.headerImage, res.biography, res.dob, res.joined, res.accountType, res.followersCount, res.followingCount);
 
     }
 }
