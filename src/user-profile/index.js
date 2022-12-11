9e2e9eda62d7eb12a020dfd127d94494c0fa09ba
taasha10 <sharmapratyasha1@gmail.com>
2022-12-11 05:23:21 -0500

    added buttons to edit-profile

--- src/user-profile/index.js
+++ src/user-profile/index.js
@@ -66,39 +66,6 @@ const UserInfo = () => {
                 </div>
 
             </div>
-
-
-
-
-
-        {/*    <div className="container bg-white rounded-3 overflow-hidden">*/}
-        {/*<div className={"row"}>*/}
-        {/*    <div className={"col-2 mt-3"}>*/}
-
-        {/*        <img src={`/images/https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpgQf7qhC2axV7ByRudRC8JpTw6epaOJgEcCxmJZiKiQ&s`} className="img-fluid rounded-circle ms-3 me-3" />*/}
-        {/*    </div>*/}
-
-
-
-        {/*    <div className="d-flex float-end col-10">*/}
-
-        {/*        <div>*/}
-        {/*            <p className="mt-3 ps-2 fw-bold">{user.name}</p>*/}
-        {/*            <p className="small mt-0 ps-2 text-muted">{user.email}</p>*/}
-        {/*            <p className="text-muted ps-2">*/}
-        {/*                <i className="text-success fa fa-calendar " /> Joined: {user.joined}</p>*/}
-        {/*            <p className="text-muted ps-2">*/}
-        {/*                <i className=" text-success fa fa-cake-candles" /> Born: {user.dob}*/}
-        {/*            </p>*/}
-        {/*            <Link to="/edit-profile" type="button" className="ps-2 btn btn-sm btn-success text-light fw-bold rounded-pill">Edit Profile</Link>*/}
-        {/*        </div>*/}
-        {/*    </div>*/}
-
-
-
-        {/*</div>*/}
-
-        {/*</div>*/}
             </div>
 
     );
