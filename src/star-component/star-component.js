8957b2f5a1eb2b11617ed51ca1ffb7aeab1b630d
Saket Kumar <kumar.sak@northeastern.edu>
2022-12-14 18:20:20 -0500

    Fixed the star componennt

--- src/star-component/star-component.js
+++ src/star-component/star-component.js
@@ -1,4 +1,3 @@
-import "./startComponent.css";
 import { useEffect, useState } from "react";
 
 const StarComponent = ({ disabled, rating = 3.6, onChange }) => {
@@ -19,20 +18,19 @@ const StarComponent = ({ disabled, rating = 3.6, onChange }) => {
           className="rating-wrapper align-self-center border-5 d-inline-flex pt-2 pe-3 "
           dir="rtl"
         >
-          <input
-            type="radio"
-            id="5-star-rating"
-            name="star-rating"
-            className="h-100 w-100 d-none"
-            disabled={disabled}
-            onChange={() => ratingChanged(5)}
-            checked={currentRating > 4}
-          />
           <label
-            htmlFor="5-star-rating"
-            className="star-rating d-inline-flex  pt-4 ps-2"
+            className="st-label  d-inline-flex  pt-4 ps-2"
             role="button"
+            data-checked={currentRating > 4}
           >
+            <input
+              type="radio"
+              name="star-rating"
+              className="h-100 w-100 d-none"
+              disabled={disabled}
+              onChange={() => ratingChanged(5)}
+              checked={currentRating > 4}
+            />
             {(currentRating > 4.5 || currentRating <= 4) && (
               <i className="fas fa-star d-inline-block"></i>
             )}
@@ -41,20 +39,20 @@ const StarComponent = ({ disabled, rating = 3.6, onChange }) => {
             )}{" "}
           </label>
 
-          <input
-            type="radio"
-            id="4-star-rating"
-            checked={currentRating > 3 && currentRating <= 4}
-            name="star-rating"
-            disabled={disabled}
-            onChange={() => ratingChanged(4)}
-            className="h-100 w-100 d-none"
-          />
+
           <label
-            htmlFor="4-star-rating"
-            className="star-rating star  d-inline-flex  pt-4 ps-2"
+            className="st-label star  d-inline-flex  pt-4 ps-2"
             role="button"
+            data-checked={currentRating > 3 && currentRating <= 4}
           >
+            <input
+              type="radio"
+              checked={currentRating > 3 && currentRating <= 4}
+              name="star-rating"
+              disabled={disabled}
+              onChange={() => ratingChanged(4)}
+              className="h-100 w-100 d-none"
+            />
             {(currentRating > 3.5 || currentRating <= 3) && (
               <i className="fas fa-star d-inline-block"></i>
             )}
@@ -63,20 +61,19 @@ const StarComponent = ({ disabled, rating = 3.6, onChange }) => {
             )}
           </label>
 
-          <input
-            type="radio"
-            id="3-star-rating"
-            checked={currentRating > 2 && currentRating <= 3}
-            disabled={disabled}
-            onChange={() => ratingChanged(3)}
-            name="star-rating"
-            className="h-100 w-100 d-none"
-          />
           <label
-            htmlFor="3-star-rating"
-            className="star-rating star  d-inline-flex  pt-4 ps-2"
+            className="st-label star  d-inline-flex  pt-4 ps-2"
             role="button"
+            data-checked={currentRating > 2 && currentRating <= 3}
           >
+            <input
+              type="radio"
+              checked={currentRating > 2 && currentRating <= 3}
+              disabled={disabled}
+              onChange={() => ratingChanged(3)}
+              name="star-rating"
+              className="h-100 w-100 d-none"
+            />
             {(currentRating > 2.5 || currentRating <= 2) && (
               <i className="fas fa-star d-inline-block"></i>
             )}
@@ -85,20 +82,20 @@ const StarComponent = ({ disabled, rating = 3.6, onChange }) => {
             )}
           </label>
 
-          <input
-            type="radio"
-            id="2-star-rating"
-            checked={currentRating > 1 && currentRating <= 2}
-            disabled={disabled}
-            onChange={() => ratingChanged(2)}
-            name="star-rating"
-            className="h-100 w-100 d-none"
-          />
+
           <label
-            htmlFor="2-star-rating"
-            className="star-rating star  d-inline-flex  pt-4 ps-2"
+            className="st-label star  d-inline-flex  pt-4 ps-2"
             role="button"
+            data-checked={currentRating > 1 && currentRating <= 2}
           >
+            <input
+              type="radio"
+              checked={currentRating > 1 && currentRating <= 2}
+              disabled={disabled}
+              onChange={() => ratingChanged(2)}
+              name="star-rating"
+              className="h-100 w-100 d-none"
+            />
             {(currentRating > 1.5 || currentRating <= 1) && (
               <i className="fas fa-star d-inline-block"></i>
             )}
@@ -107,20 +104,20 @@ const StarComponent = ({ disabled, rating = 3.6, onChange }) => {
             )}{" "}
           </label>
 
-          <input
-            type="radio"
-            id="1-star-rating"
-            checked={currentRating > 0 && currentRating <= 1}
-            disabled={disabled}
-            onChange={() => ratingChanged(1)}
-            name="star-rating"
-            className="h-100 w-100 d-none"
-          />
+
           <label
-            htmlFor="1-star-rating"
-            className="star-rating star d-inline-flex pt-4 ps-2"
+            className="st-label star d-inline-flex pt-4 ps-2"
             role="button"
+            data-checked={currentRating > 0 && currentRating <= 1}
           >
+            <input
+              type="radio"
+              checked={currentRating > 0 && currentRating <= 1}
+              disabled={disabled}
+              onChange={() => ratingChanged(1)}
+              name="star-rating"
+              className="h-100 w-100 d-none"
+            />
             {(currentRating > 0.5 || currentRating == 0) && (
               <i className="fas fa-star d-inline-block"></i>
             )}
