83f14023b590b81e99f6076af9815a0aee97c813
JOEL-8899 <56861930+JOEL-8899@users.noreply.github.com>
2022-12-14 20:04:28 -0500

    implemented the my review section in user profile

--- src/user-profile/my-reviews.js
+++ src/user-profile/my-reviews.js
@@ -1,8 +1,20 @@
-const MyReviews = () => {
+import { useEffect, useState } from "react";
+import ReviewList from "../reviews/reviews-list";
+import { findReviewByUserId } from "../services/user-review-services";
 
+const MyReviews = ({ user }) => {
+  const [reviews, setReviewList] = useState([]);
+
+  useEffect(() => {
+    findReviewByUserId(user._id).then((reviewList) => {
+      setReviewList(reviewList);
+    });
+  }, [user]);
   return (
-    <div></div>
-  )
-}
+    <div>
+      <ReviewList reviewList={reviews} />
+    </div>
+  );
+};
 
 export default MyReviews;
