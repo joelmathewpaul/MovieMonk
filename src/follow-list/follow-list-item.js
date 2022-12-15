380a180960263efcdd4c3cbbea4776c2153b4f42
taasha10 <sharmapratyasha1@gmail.com>
2022-12-15 18:17:36 -0500

    navigating to user profile

--- src/follow-list/follow-list-item.js
+++ src/follow-list/follow-list-item.js
@@ -1,14 +1,15 @@
 import {useNavigate} from "react-router";
 import {deleteFollowing} from "../services/follow-service";
 import {useSelector} from "react-redux";
+import {Link} from "react-router-dom";
 
 const FollowListItem = ({followUser,cancelRequired}) => {
 
     const navigate = useNavigate();
     const profileUser = useSelector(state => state.user);
     const onUserItemClick = () => {
-        let nameEncoded = followUser.name.replace(/\s/g, "-").toLowerCase();
-        navigate(`/user/${followUser._id}/${nameEncoded}`);
+        // let nameEncoded = followUser.name.replace(/\s/g, "-").toLowerCase();
+        navigate(`/view-profile/${followUser._id}`);
     }
     const onCancelItemClick = async () => {
         console.log("inside oncancel",followUser,profileUser)
@@ -19,19 +20,17 @@ const FollowListItem = ({followUser,cancelRequired}) => {
 
     <div className="d-flex flex-row align-items-center bg-white m-2 p-2">
 
-        <div onClick={onUserItemClick}>
+        <div>
             <img src={followUser.profilePhoto} width={'50px'} height={'50px'} alt="username"
                  className="rounded-circle"/>
         </div>
-        <div className="flex-fill ps-3">
+        <div className="flex-fill ps-3" onClick={onUserItemClick}>
             <span className="text-primary">{followUser.name}</span>
-
-            {cancelRequired && <span className="float-end" onClick={onCancelItemClick}><i
-                className="fa-solid fa-xmark"/></span>}
-
         </div>
-    </div>
+        {cancelRequired && <span className="float-end" onClick={onCancelItemClick}><i
+            className="fa-solid fa-xmark"/></span>}
 
+    </div>
 
     )
 }
