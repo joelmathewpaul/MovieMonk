e7cc035bb6c4d446e0a5ab3e0c6573ab627b0dd6
taasha10 <sharmapratyasha1@gmail.com>
2022-12-15 17:57:04 -0500

    removed message

--- src/follow-list/index.js
+++ src/follow-list/index.js
@@ -11,16 +11,15 @@ const FollowList = ({ followList = [],cancelRequired }) => {
 
         <div className="row">
             {
-                followList &&
-                (followList.map((user) => {
+                followList.map((user) => {
                     return (
                         <div key={`${user.id}`} className="col-md-6 col-xl-4 mt-3">
                             <FollowListItem followUser={user} cancelRequired={cancelRequired} />
                         </div>
                     )
-                }))
+                })
             }
-            {!followList && <h2>You are currently not following any user</h2>}
+
 
         </div>
 
