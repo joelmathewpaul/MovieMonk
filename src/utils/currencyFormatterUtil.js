fff4e8aa947024b3775be6ab1bbe8b4434d3fdb2
JOEL-8899 <56861930+JOEL-8899@users.noreply.github.com>
2022-12-16 14:59:37 -0500

    implemented the function for currency conversion

--- src/utils/currencyFormatterUtil.js
+++ src/utils/currencyFormatterUtil.js
@@ -1,2 +1,9 @@
-const CurrencyFormatter = (currency) => {};
+const CurrencyFormatter = (currency) => {
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  return formatter.format(currency);
+};
 export default CurrencyFormatter;
