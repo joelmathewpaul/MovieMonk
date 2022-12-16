e9487f34a291d3dcc792ab272b01c04218638e98
JOEL-8899 <56861930+JOEL-8899@users.noreply.github.com>
2022-12-16 00:01:30 -0500

    implemented navigating back to login on sign out

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import { Route, BrowserRouter, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './home';
-import LoginSignup from './login';
-import MovieDetails from './movie-details';
-import Search from './search';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./home";
+import LoginSignup from "./login";
+import MovieDetails from "./movie-details";
+import Search from "./search";
 import { useDispatch } from "react-redux";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { getProfile } from "./services/auth-service";
-import { saveUser } from './reducers/user-reducer';
+import { saveUser } from "./reducers/user-reducer";
 import Profile from "./user-profile";
-import User from './models/user';
-import ReadOnlyUserInfo from './user-profile/read-only-information';
+import User from "./models/user";
+import ReadOnlyUserInfo from "./user-profile/read-only-information";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    getProfile().then(dbUser => {
-      const modelUser = User.getUserDetails(dbUser);
-      dispatch(saveUser(modelUser));
-    }).catch(error => {
-      //user is not logged in
-    });
+    getProfile()
+      .then((dbUser) => {
+        const modelUser = User.getUserDetails(dbUser);
+        dispatch(saveUser(modelUser));
+      })
+      .catch((error) => {
+        //user is not logged in
+      });
   }, []);
 
   return (
@@ -32,7 +34,11 @@ function App() {
           <Route exact path="/movie/:id/:title" element={<MovieDetails />} />
           <Route exact path="/search" element={<Search />} />
           <Route exact path="/profile/*" element={<Profile />} />
-          <Route exact path="/view-profile/:uid" element={<ReadOnlyUserInfo />} />
+          <Route
+            exact
+            path="/view-profile/:uid"
+            element={<ReadOnlyUserInfo />}
+          />
           <Route path="/*" element={<Home />} />
         </Routes>
       </div>
